a5edb4d838e10d6e16a198fb367718ee38313853
Tom <tgclayton1@gmail.com>
2021-01-11 09:04:30 +1300

    add min width to center column

--- src/App.js
+++ src/App.js
@@ -6,9 +6,10 @@ import { canvasDraw } from './functions/canvas.js'
 
 function App() {
   const [size, setSize] = useState(100)
-  const [tileSize, setTileSize] = useState(500 / size)
+  const [tileSize, setTileSize] = useState(5)
   const [field, setField] = useState(createField(size, tileSize))
   const [liveCells, setLiveCells] = useState(newMakeRandomMap(size))
+  const [showTestControls, setShowTestControls] = useState(false)
 
   useEffect(() => {
     canvasDraw(field, liveCells, tileSize)
@@ -17,7 +18,7 @@ function App() {
   function changeSize() {
     const size = document.getElementById('size-change').value
     const tileSize = 500 / size
-    setLiveCells(newMakeRandomMap(size))
+    setLiveCells([])
     setField(createField(size, tileSize))
     setSize(size)
     setTileSize(tileSize)
@@ -43,11 +44,16 @@ function App() {
       </header>
       <div className="app-body">
         <div id='left-column' className="column">
-          <button onClick={() => canvasTest(canvasDraw)}>Canvas Test</button>
-          {/* <button onClick={() => checkState()}>Check State</button> */}
-          <br></br>
-          <input id='size-change' type='number' onSubmit ></input>
-          <button onClick={() => changeSize()} >Change field size</button>
+          <button onClick = {() => setShowTestControls(!showTestControls)}>Toggle Test Controls</button>
+          <div id="test-controls" className ="column">
+            <button onClick={() => canvasTest(canvasDraw)}>Canvas Test</button>
+            {/* <button onClick={() => checkState()}>Check State</button> */}
+            <br></br>
+            <input id='size-change' type='number' onSubmit ></input>
+            <button onClick={() => changeSize()} >Change field size</button>
+            <br></br>
+            {/* <button onClick = {() => setShowTestControls(false)}>Hide Panel</button> */}
+          </div>
         </div>
 
         <div id='center-column' className="column">
