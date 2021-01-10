6375dda71a13c596217a7e7288ca6d7443164a4f
Tom <tgclayton1@gmail.com>
2021-01-11 09:07:44 +1300

    create testcontrols component

---
+++ src/components/TestControls.jsx
@@ -0,0 +1,13 @@
+export default function TestControls () {
+  // return (
+  //   <div id="test-controls" className ="column">
+  //   <button onClick={() => canvasTest(canvasDraw)}>Canvas Test</button>
+  //   {/* <button onClick={() => checkState()}>Check State</button> */}
+  //   <br></br>
+  //   <input id='size-change' type='number' onSubmit ></input>
+  //   <button onClick={() => changeSize()} >Change field size</button>
+  //   <br></br>
+  //   {/* <button onClick = {() => setShowTestControls(false)}>Hide Panel</button> */}
+  // </div>
+  // )
+}
