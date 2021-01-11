77299566e6fe9a02e0aeb1b7b1a8c290ec2b2d13
Tom <tgclayton1@gmail.com>
2021-01-12 08:17:35 +1300

    test panel toggleable

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import './App.css';
 import Game from './components/Game'
+import TestControls from './components/TestControls'
 import React, { useState, useEffect } from "react";
 import { createField, newMakeRandomMap } from './functions/game.js'
 import { canvasDraw } from './functions/canvas.js'
+import {} from './functions/app.js'
 
 function App() {
   const [size, setSize] = useState(100)
   const [tileSize, setTileSize] = useState(5)
   const [field, setField] = useState(createField(size, tileSize))
-  const [liveCells, setLiveCells] = useState(newMakeRandomMap(size))
+  const [liveCells, setLiveCells] = useState([])
   const [showTestControls, setShowTestControls] = useState(false)
 
   useEffect(() => {
@@ -24,17 +26,15 @@ function App() {
     setTileSize(tileSize)
   }
 
-  function canvasTest(callback) {
+  function canvasTest() {
     const randMap = newMakeRandomMap(size)
     setLiveCells(randMap)
-    callback(field, liveCells, tileSize)
+    canvasDraw(field, liveCells, tileSize)
   }
 
-  function checkState() {
-    console.log('size:', size)
-    console.log('tileSize:', tileSize)
-    console.log('field:', field)
-    console.log('liveCells:', liveCells)
+  let testControlPanel = null
+  if (showTestControls) {
+    testControlPanel = <TestControls canvasTest={canvasTest} changeSize={changeSize} />
   }
 
   return (
@@ -44,25 +44,25 @@ function App() {
       </header>
       <div className="app-body">
         <div id='left-column' className="column">
-          <button onClick = {() => setShowTestControls(!showTestControls)}>Toggle Test Controls</button>
-          <div id="test-controls" className ="column">
+          <button onClick={() => setShowTestControls(!showTestControls)}>Toggle Test Controls</button>
+          {testControlPanel}
+          {/* <div id="test-controls" className="column">
             <button onClick={() => canvasTest(canvasDraw)}>Canvas Test</button>
-            {/* <button onClick={() => checkState()}>Check State</button> */}
             <br></br>
             <input id='size-change' type='number' onSubmit ></input>
             <button onClick={() => changeSize()} >Change field size</button>
             <br></br>
-            {/* <button onClick = {() => setShowTestControls(false)}>Hide Panel</button> */}
-          </div>
+          </div> */}
         </div>
 
         <div id='center-column' className="column">
-          <Game></Game>
+          <Game />
         </div>
 
         <div id='right-column' className="column">
 
         </div>
+
       </div>
     </div>
   );
--- src/components/TestControls.jsx
+++ src/components/TestControls.jsx
@@ -1,13 +1,12 @@
-export default function TestControls () {
-  // return (
-  //   <div id="test-controls" className ="column">
-  //   <button onClick={() => canvasTest(canvasDraw)}>Canvas Test</button>
-  //   {/* <button onClick={() => checkState()}>Check State</button> */}
-  //   <br></br>
-  //   <input id='size-change' type='number' onSubmit ></input>
-  //   <button onClick={() => changeSize()} >Change field size</button>
-  //   <br></br>
-  //   {/* <button onClick = {() => setShowTestControls(false)}>Hide Panel</button> */}
-  // </div>
-  // )
+export default function TestControls(props) {
+  return (
+    <div id="test-controls" className="column">
+      <button onClick={() => props.canvasTest()}>Canvas Test</button>
+      <br></br>
+      <input id='size-change' type='number' onSubmit ></input>
+      <button onClick={() => props.changeSize()} >Change field size</button>
+      <br></br>
+      {/* <button onClick = {() => setShowTestControls(false)}>Hide Panel</button> */}
+    </div>
+  )
 }
