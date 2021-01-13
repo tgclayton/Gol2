bae86c2296e06262313e97080fa25e482142b882
Tom <tgclayton1@gmail.com>
2021-01-13 21:30:32 +1300

    add title to speed control panel

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -3,12 +3,12 @@ import { gridDraw, clearCanvas } from '../functions/canvas.js'
 
 export default function Controls(props) {
   const [showGrid, setShowGrid] = useState(true)
-  useEffect(()=> {
+  useEffect(() => {
     gridDraw(props.tileSize, props.boardSize)
-  },[])
+  }, [])
 
   useEffect(() => {
-    const activeSpeed = props.speed ===600? 1 :props.speed === 300? 2:props.speed === 120? 3: props.speed === 30? 4: 5
+    const activeSpeed = props.speed === 600 ? 1 : props.speed === 300 ? 2 : props.speed === 120 ? 3 : props.speed === 30 ? 4 : 5
     const buttons = Array.from(document.getElementsByClassName('speed-button'))
     buttons.forEach(element => element.classList.remove('active-speed'))
     document.getElementById(`speed-button-${activeSpeed}`).classList.add('active-speed')
@@ -39,7 +39,7 @@ export default function Controls(props) {
       <div id='main-control-panel' className='control-panel'>
         <button onClick={() => props.makeRandomStart()} >Random Start</button>
         <button onClick={() => props.clearGame()} >Clear Game</button>
-        <br/>
+        <br />
         <button onClick={() => props.nextGen()} >Next Generation</button>
         <br /><br />
         <input id='size-change' type='number' defaultValue={props.boardSize} max={150}></input>
@@ -49,14 +49,17 @@ export default function Controls(props) {
         <button onClick={() => toggleGrid()}>Toggle Grid</button>
         <button onClick={() => props.wrap.toggleWrap(!props.wrap.wrap)}>Toggle Edge Wrapping</button>
       </div>
-      <div id='speed-control' className='control-panel flex-row'>
-        <p>Slower</p>
-        <div className='speed-button' id ={`speed-button-1`} onClick = {() => props.changeSpeed(600)}>1</div>
-        <div className='speed-button' id ={`speed-button-2`} onClick = {() => props.changeSpeed(300)}>2</div>
-        <div className='speed-button' id ={`speed-button-3`} onClick = {() => props.changeSpeed(120)}>3</div>
-        <div className='speed-button' id ={`speed-button-4`} onClick = {() => props.changeSpeed(30)}>4</div>
-        <div className='speed-button' id ={`speed-button-5`} onClick = {() => props.changeSpeed(1)}>5</div>
-        <p>Faster</p>
+      <div id='speed-control' className = 'control-panel column'>
+        <h5>Speed Control</h5>
+        <div className='flex-row'>
+          <p>Slower</p>
+          <div className='speed-button' id={`speed-button-1`} onClick={() => props.changeSpeed(600)}>1</div>
+          <div className='speed-button' id={`speed-button-2`} onClick={() => props.changeSpeed(300)}>2</div>
+          <div className='speed-button' id={`speed-button-3`} onClick={() => props.changeSpeed(120)}>3</div>
+          <div className='speed-button' id={`speed-button-4`} onClick={() => props.changeSpeed(30)}>4</div>
+          <div className='speed-button' id={`speed-button-5`} onClick={() => props.changeSpeed(1)}>5</div>
+          <p>Faster</p>
+        </div>
       </div>
     </>
   )
