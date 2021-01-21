ec62f5042ea2ff896f409520e64859188e3bfedd
Tom <tgclayton1@gmail.com>
2021-01-22 09:08:16 +1300

    styling changes

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -25,20 +25,24 @@ export default function Info(props) {
     <>
       <div id="info-panel" className="control-panel">
         <h5 className='panel-title'>Game Info</h5>
+        <div style={{textAlign: '', width:'fit-content', margin:'auto'}}>
         <p >Live Cells: <span id='livecell-info'>{props.liveCells.size}</span></p>
         <p >Generation: <span id='gen-info'>{props.gen}</span></p>
         <p>Field Size: {props.size} x {props.size}</p>
         <p>Edge Wrapping: <span id='wrap-indicator'>{wrap}</span> </p>
         <p>Speed: <span id='speed-indicator'>{speedDisp}</span></p>
+        </div>
       </div>
       <div className='control-panel instructions' id = "key-control-instructions" style={{marginTop: '1em'}}>
         <h5 className='panel-title'>Keyboard Controls</h5>
+        <div style={{textAlign: 'justify',}}>
         <p><span>W/Up Arrow:</span> Create a new random game</p>
         <p><span>S/Down Arrow:</span> Clear the game board</p>
-        <p><span>D/Left Arrow:</span> Advance the game a single generation</p>
-        <p><span>Space:</span> Starts or pauses the game running</p>
+        <p><span>D/Left Arrow:</span> Run a single generation</p>
+        <p><span>Space:</span> Start or pause the game</p>
         <p><span>Numbers 1-5:</span> Set game speed</p>
         <p><span>E:</span> Toggle edge wrapping</p>
+        </div>
       </div>
     </>
   )
--- src/components/Instructions.jsx
+++ src/components/Instructions.jsx
@@ -1,17 +1,26 @@
 export default function Instructions(props) {
   return (
     <>
-      <div className='control-panel instructions' >
-      <h5>Rules</h5>
-        <p>Conway's Game of Life consists of 2d game board composed of square cells.</p>
+      <div className='control-panel instructions' id='game-instructions' style={{
+        letterSpacing:'-0.001em'
+      }} >
+        {/* <h5 className={'panel-title'}>Rules</h5> */}
+        <p>Conway's Game of Life consists of 2D game board composed of square cells. Each cell can
+        exist in one of two states, alive(green) and dead(grey). Once the starting state of the
+        board is determined the game proceeds in a series of generations, during which cells may
+          change state according to three rules.</p>
+        <p style={{marginBottom:'0'}}>1: Any live cell with two or three live neighbours survives.</p>
+        <p style={{marginBottom:'0'}}>2: Any dead cell with three live neighbours becomes a live cell.</p>
+        <p>3: All other live cells die or stay dead.</p>
+
         <p>While the game is not running the game cells may be painted on or removed
-           from the board by clicking and dragging with the mouse. The left mouse button will turn
-           cells live and the right button will turn them dead. Holding the shift key while dragging
+        from the board by clicking and dragging with the mouse. The left mouse button will turn
+        cells live and the right button will turn them dead. Holding the shift key while dragging
            will cause a rectangular block of cells to be affected. </p>
-           <p>While edge wrapping is on cells on the edges of the map may count cells on the opposite
-             edge as their neighbours, all cells on the board have the same number of neighbours. 
-             While it is off the board edge act as a hard barrier and cells on the edge have less neighbours
-             than other cells.
+
+        <p>While edge wrapping is on cells on the edges of the map may count cells on the opposite
+        edge as their neighbours.While it is off the board edge act as a hard barrier and cells on
+        the edge have no neighbours off the board edge.
            </p>
       </div>
 
